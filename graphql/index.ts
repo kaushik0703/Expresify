7c49b34f915a80f95bc340cfaa28dad6905b5b3b
kaushik0703 <106008167+kaushik0703@users.noreply.github.com>
2023-07-23 11:35:02 +0530

    Update index.ts

--- graphql/index.ts
+++ graphql/index.ts
@@ -55,7 +55,7 @@ export const createUserMutation = `
 `;
 
 export const projectsQuery = `
-  query getProjects($category: String, $endCursor: String) {
+  query getProjects($endCursor: String) {
     projectSearch(first: 4, after: $endCursor, filter: {category: {eq: $category}}) {
       pageInfo {
         hasNextPage
