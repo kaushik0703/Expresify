666859804047edb1afb6424551c4582005e6a644
kaushik0703 <106008167+kaushik0703@users.noreply.github.com>
2023-07-23 16:36:35 +0530

    Update ProjectForm.tsx

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -31,10 +31,12 @@ const ProjectForm = ({type, session, project}: Props) => {
             if(type === 'create') {
                 await createNewProject(form, session?.user?.id as string, token);
                 router.push('/')
+                router.refresh();
             }
             else if(type === 'edit') {
                 await editProject(form, project?.id as string, token);
                 router.push('/')
+                router.refresh();
             }
         } catch (error) {
             console.log(error);
