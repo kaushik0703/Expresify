52b9c7a80831626f4cfe58d006430d284fe84984
Kaushik Moralwar <106008167+kaushik0703@users.noreply.github.com>
2024-10-19 10:06:07 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/Navbar'; 
 import './globals.css';
 import Footer from '@/components/Footer';
+
 export const metadata = {
   title: "Expresify",
   description: "Showcase and discover remarkable developer Projects",
@@ -14,6 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body>
         <Navbar />
         <main>
